bdb63171f016042bd3829321c816cbf66aea71d8
Andrei Belokopytov <belokopytov87@gmail.com>
2022-11-19 01:05:47 +0300

    Implement checking if a wallet is connected

--- Web/layouts/DefaultLayout.jsx
+++ Web/layouts/DefaultLayout.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { useRouter } from "next/router";
+import { useAccount } from "wagmi";
+import { useEffect } from "react";
 
 function DefaultLayout({ children }) {
+  const { isConnected } = useAccount();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isConnected) {
+      router.push("./");
+    }
+  }, [isConnected, router]);
   return (
     <div>
       <Navbar />
       {children}
-     <Footer/>
+      <Footer />
     </div>
   );
 }
